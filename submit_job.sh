318a9e82e3d3520872f2a57db3eb0b82202c8146
Armin Sobhani <arminms@gmail.com>
2018-08-07 12:58:44 -0400

    change submit_job.sh to use new reservations

--- submit_job.sh
+++ submit_job.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+#SBATCH --reservation=dl_uoit_wr      # reservation name to use
+#SBATCH --account=dl_uoit_wa          # account name to use
 #SBATCH --gres=gpu:1                  # Number of GPUs (per node)
-#SBATCH --mem=4000M                   # memory (per node)
+#SBATCH --mem=4G                      # memory (per node)
 #SBATCH --time=0-00:10                # time (DD-HH:MM)
 #SBATCH --output=slurm-%j.out         # output filename pattern; j == jobid
 source activate pytorch4
